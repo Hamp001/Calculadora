b7eaf6a5f8eda135609434a3a08ecb23d19498ad
hugo <hugoalomp@gmail.com>
2023-08-22 23:05:40 -0600

    numeros listos

---
+++ ventana.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+
+class InterfazGrafica:
+    def __init__(self, nombre_ventana):
+        self.nombre_ventana = nombre_ventana
+        self.root = tk.Tk()
+        self.root.title(nombre_ventana)
+
+        self.label = tk.Label(self.root, text="¡Hola desde la interfaz gráfica!",width=5,height=2,font=("Helvetica", 16),anchor="w")
+        #"ew" significa que el widget se expandirá horizontalmente para ocupar el espacio disponible en su columna.
+        self.label.grid(row=0, column=0, columnspan=5, sticky="ew")
+        self.mostrar_cuadricula()
+
+    def mostrar_cuadricula(self):
+        cont=7
+        for i in range(4):
+            for j in range(4):
+                #colorear esquina inferior derecha e izquierda 
+                if(i == 3 and (j == 2)):
+                    frame = tk.Frame(self.root, bg="grey", width=120, height=120)
+                else:
+                    frame = tk.Frame(self.root, bg="#e98c05", width=120, height=120)
+                    label = tk.Label(frame, text=str(cont), fg="white", bg="#e98c05",font=("Helvetica", 24))
+                    label.place(relx=0.5, rely=0.5, anchor="center")  # Coloca el label en el centro del frame
+                    
+                    #mostrar numeros en orden de teclado numerico 7894561230...
+                    if(cont==9):
+                        cont=4
+                    elif(cont==6):
+                        cont=1
+                    elif(cont==3):
+                        cont=0
+                    else:           
+                        cont+=1
+                
+                frame.grid(row=i+1, column=j)# con bordes :frame.grid(row=i+1, column=j,padx=1, pady=1)
+                #label.pack()
+
+    def mostrar_ventana(self):
+        # se ejecuta infinitamente para mostrar los eventos en la ventana
+        self.root.mainloop()
+
+
+def main():
+    ventana = InterfazGrafica("Calculadora")
+    ventana.mostrar_ventana()
+if __name__ == "__main__":
+    main()
