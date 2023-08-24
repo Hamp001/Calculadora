6bc926671b845837edc2caf16ecd75c015b3089d
hugo <hugoalomp@gmail.com>
2023-08-23 23:08:55 -0600

    trabajando en labels de operaciones

--- ventana.py
+++ ventana.py
@@ -16,13 +16,26 @@ class InterfazGrafica:
         for i in range(4):
             for j in range(4):
                 #colorear esquina inferior derecha e izquierda 
-                if(i == 3 and (j == 2)):
+                if(i == 3 and (j == 2 or j==1)):
                     frame = tk.Frame(self.root, bg="grey", width=120, height=120)
-                else:
-                    frame = tk.Frame(self.root, bg="#e98c05", width=120, height=120)
-                    label = tk.Label(frame, text=str(cont), fg="white", bg="#e98c05",font=("Helvetica", 24))
+                elif(i==0 and j==3):
+                    frame = tk.Frame(self.root, bg="white", width=120, height=120)
+                    label = tk.Label(frame, text="/", fg="black",font=("Helvetica", 24))
                     label.place(relx=0.5, rely=0.5, anchor="center")  # Coloca el label en el centro del frame
                     
+                elif(i==1 and j==3):
+                    frame = tk.Frame(self.root, bg="white", width=120, height=120)
+                    label = tk.Label(frame, text="*", fg="white", bg="blue",font=("Helvetica", 24))
+                elif(i==2 and j==3):
+                    frame = tk.Frame(self.root, bg="white", width=120, height=120)
+                    label = tk.Label(frame, text="+", fg="white", bg="blue",font=("Helvetica", 24))
+                elif(i==3 and j==3):
+                    frame = tk.Frame(self.root, bg="white", width=120, height=120)
+                    label = tk.Label(frame, text="-", fg="white", bg="blue",font=("Helvetica", 24))
+                else:
+                    frame = tk.Frame(self.root, bg="#e98c05", width=120, height=120)#cuadros donde va cada numero
+                    label = tk.Label(frame, text=str(cont), fg="white", bg="#e98c05",font=("Helvetica", 24))#establece numero en el label
+                    label.place(relx=0.5, rely=0.5, anchor="center")  # Coloca el label en el centro del frame
                     #mostrar numeros en orden de teclado numerico 7894561230...
                     if(cont==9):
                         cont=4
