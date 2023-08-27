e79448327e67fb771418eecfa1a3ee1d11710ef2
hugo <hugoalomp@gmail.com>
2023-08-26 20:26:33 -0600

    botones listos

--- ventana.py
+++ ventana.py
@@ -11,8 +11,6 @@ class InterfazCalculadora:
         self.mostrar_cuadricula()
 
     def mostrar_cuadricula(self):
-        cont=7
-
         #Renglones
         for i in range(4):
             #columnas
@@ -84,27 +82,119 @@ class InterfazCalculadora:
                     frame.bind("<Button-1>",lambda event: self.boton_click("+"))
 
                     label.place(relx=0.5, rely=0.5, anchor="center")  # Coloca el label en el centro del frame                
-                else:
+                #NUMEROS:
+                elif(i==0 and j==0):
                     frame = tk.Frame(self.root, bg="#e98c05", width=120, height=120)#cuadros donde va cada numero
-                    label = tk.Label(frame, text=str(cont), fg="white", bg="#e98c05",font=("Helvetica", 24,"bold"))#establece numero en el label
+                    label = tk.Label(frame, text="7", fg="white", bg="#e98c05",font=("Helvetica", 24,"bold"))#establece numero en el label
                     
-                    print(cont)
+                    #evento click izquierdo "<Button-1>"
+                    # En este caso, estamos creando una función anonima (lambda) que toma un parámetro llamado event.
+                    # Esta función lambda se utilizará como la función que se ejecutará cuando ocurra el evento
+                    label.bind("<Button-1>", lambda event: self.boton_click("7"))
+                    frame.bind("<Button-1>",lambda event: self.boton_click("7"))
 
-                    
-                    
                     label.place(relx=0.5, rely=0.5, anchor="center")  # Coloca el label en el centro del frame
-                    #mostrar numeros en orden de teclado numerico 7894561230...
-                    if(cont==9):
-                        cont=4
-                    elif(cont==6):
-                        cont=1
-                    elif(cont==3):
-                        cont=0
-                    else:       
-                        label.bind("<Button-1>", lambda event: self.boton_click(str(cont)))
-                        frame.bind("<Button-1>",lambda event: self.boton_click(str(cont)))
-                        print("Print en el else"+str(cont))    
-                        cont+=1
+
+                elif(i==0 and j==1):
+                    frame = tk.Frame(self.root, bg="#e98c05", width=120, height=120)#cuadros donde va cada numero
+                    label = tk.Label(frame, text="8", fg="white", bg="#e98c05",font=("Helvetica", 24,"bold"))#establece numero en el label
+                    
+                    #evento click izquierdo "<Button-1>"
+                    # En este caso, estamos creando una función anonima (lambda) que toma un parámetro llamado event.
+                    # Esta función lambda se utilizará como la función que se ejecutará cuando ocurra el evento
+                    label.bind("<Button-1>", lambda event: self.boton_click("8"))
+                    frame.bind("<Button-1>",lambda event: self.boton_click("8"))  
+
+                    label.place(relx=0.5, rely=0.5, anchor="center")  # Coloca el label en el centro del frame
+                elif(i==0 and j==2):
+                    frame = tk.Frame(self.root, bg="#e98c05", width=120, height=120)#cuadros donde va cada numero
+                    label = tk.Label(frame, text="9", fg="white", bg="#e98c05",font=("Helvetica", 24,"bold"))#establece numero en el label
+                    
+                    #evento click izquierdo "<Button-1>"
+                    # En este caso, estamos creando una función anonima (lambda) que toma un parámetro llamado event.
+                    # Esta función lambda se utilizará como la función que se ejecutará cuando ocurra el evento
+                    label.bind("<Button-1>", lambda event: self.boton_click("9"))
+                    frame.bind("<Button-1>",lambda event: self.boton_click("9"))  
+
+                    label.place(relx=0.5, rely=0.5, anchor="center")  # Coloca el label en el centro del frame
+                elif(i==1 and j==0):
+                    frame = tk.Frame(self.root, bg="#e98c05", width=120, height=120)#cuadros donde va cada numero
+                    label = tk.Label(frame, text="4", fg="white", bg="#e98c05",font=("Helvetica", 24,"bold"))#establece numero en el label
+                    
+                    #evento click izquierdo "<Button-1>"
+                    # En este caso, estamos creando una función anonima (lambda) que toma un parámetro llamado event.
+                    # Esta función lambda se utilizará como la función que se ejecutará cuando ocurra el evento
+                    label.bind("<Button-1>", lambda event: self.boton_click("4"))
+                    frame.bind("<Button-1>",lambda event: self.boton_click("4"))  
+
+                    label.place(relx=0.5, rely=0.5, anchor="center")  # Coloca el label en el centro del frame
+                elif(i==1 and j==1):
+                    frame = tk.Frame(self.root, bg="#e98c05", width=120, height=120)#cuadros donde va cada numero
+                    label = tk.Label(frame, text="5", fg="white", bg="#e98c05",font=("Helvetica", 24,"bold"))#establece numero en el label
+                    
+                    #evento click izquierdo "<Button-1>"
+                    # En este caso, estamos creando una función anonima (lambda) que toma un parámetro llamado event.
+                    # Esta función lambda se utilizará como la función que se ejecutará cuando ocurra el evento
+                    label.bind("<Button-1>", lambda event: self.boton_click("5"))
+                    frame.bind("<Button-1>",lambda event: self.boton_click("5"))  
+
+                    label.place(relx=0.5, rely=0.5, anchor="center")  # Coloca el label en el centro del frame
+                elif(i==1 and j==2):
+                    frame = tk.Frame(self.root, bg="#e98c05", width=120, height=120)#cuadros donde va cada numero
+                    label = tk.Label(frame, text="6", fg="white", bg="#e98c05",font=("Helvetica", 24,"bold"))#establece numero en el label
+                    
+                    #evento click izquierdo "<Button-1>"
+                    # En este caso, estamos creando una función anonima (lambda) que toma un parámetro llamado event.
+                    # Esta función lambda se utilizará como la función que se ejecutará cuando ocurra el evento
+                    label.bind("<Button-1>", lambda event: self.boton_click("6"))
+                    frame.bind("<Button-1>",lambda event: self.boton_click("6"))  
+
+                    label.place(relx=0.5, rely=0.5, anchor="center")  # Coloca el label en el centro del frame
+                elif(i==2 and j==0):
+                    frame = tk.Frame(self.root, bg="#e98c05", width=120, height=120)#cuadros donde va cada numero
+                    label = tk.Label(frame, text="1", fg="white", bg="#e98c05",font=("Helvetica", 24,"bold"))#establece numero en el label
+                    
+                    #evento click izquierdo "<Button-1>"
+                    # En este caso, estamos creando una función anonima (lambda) que toma un parámetro llamado event.
+                    # Esta función lambda se utilizará como la función que se ejecutará cuando ocurra el evento
+                    label.bind("<Button-1>", lambda event: self.boton_click("1"))
+                    frame.bind("<Button-1>",lambda event: self.boton_click("1"))  
+
+                    label.place(relx=0.5, rely=0.5, anchor="center")  # Coloca el label en el centro del frame
+                elif(i==2 and j==1):
+                    frame = tk.Frame(self.root, bg="#e98c05", width=120, height=120)#cuadros donde va cada numero
+                    label = tk.Label(frame, text="2", fg="white", bg="#e98c05",font=("Helvetica", 24,"bold"))#establece numero en el label
+                    
+                    #evento click izquierdo "<Button-1>"
+                    # En este caso, estamos creando una función anonima (lambda) que toma un parámetro llamado event.
+                    # Esta función lambda se utilizará como la función que se ejecutará cuando ocurra el evento
+                    label.bind("<Button-1>", lambda event: self.boton_click("2"))
+                    frame.bind("<Button-1>",lambda event: self.boton_click("2"))  
+
+                    label.place(relx=0.5, rely=0.5, anchor="center")  # Coloca el label en el centro del frame
+                elif(i==2 and j==2):
+                    frame = tk.Frame(self.root, bg="#e98c05", width=120, height=120)#cuadros donde va cada numero
+                    label = tk.Label(frame, text="3", fg="white", bg="#e98c05",font=("Helvetica", 24,"bold"))#establece numero en el label
+                    
+                    #evento click izquierdo "<Button-1>"
+                    # En este caso, estamos creando una función anonima (lambda) que toma un parámetro llamado event.
+                    # Esta función lambda se utilizará como la función que se ejecutará cuando ocurra el evento
+                    label.bind("<Button-1>", lambda event: self.boton_click("3"))
+                    frame.bind("<Button-1>",lambda event: self.boton_click("3"))  
+
+                    label.place(relx=0.5, rely=0.5, anchor="center")  # Coloca el label en el centro del frame
+                elif(i==3 and j==0):
+                    frame = tk.Frame(self.root, bg="#e98c05", width=120, height=120)#cuadros donde va cada numero
+                    label = tk.Label(frame, text="0", fg="white", bg="#e98c05",font=("Helvetica", 24,"bold"))#establece numero en el label
+                    
+                    #evento click izquierdo "<Button-1>"
+                    # En este caso, estamos creando una función anonima (lambda) que toma un parámetro llamado event.
+                    # Esta función lambda se utilizará como la función que se ejecutará cuando ocurra el evento
+                    label.bind("<Button-1>", lambda event: self.boton_click("0"))
+                    frame.bind("<Button-1>",lambda event: self.boton_click("0"))  
+
+                    label.place(relx=0.5, rely=0.5, anchor="center")  # Coloca el label en el centro del frame
+
                 frame.grid(row=i+1, column=j)# con bordes :frame.grid(row=i+1, column=j,padx=1, pady=1)
                 #label.pack()
 
